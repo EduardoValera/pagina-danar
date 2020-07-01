2593d5b99392921d4d582d395058dfd6c69d8887
EduardoValera <67677411+EduardoValera@users.noreply.github.com>
2020-07-01 01:33:38 -0500

    Add files via upload

---
+++ Uniformes/enviar.php
@@ -0,0 +1,17 @@
+<?php  
+
+	$destino= "danarmultiservicios@gmail.com";
+
+	$nombre = $_POST["nombre"];
+	$movil = $_POST["movil"];
+	$correo = $_POST["correo"];
+	$archivo = $_POST["archivo"];
+	$mensaje = $_POST["mensaje"];
+
+	$contenido = "Nombre: " . $nombre . "\nMovil: " . $movil . "\nCorreo: " . $correo . "\nArchivo: " . $archivo . "\nMensaje: " . $mensaje;
+
+	mail($destino,"Confección",$contenido);
+	header("Location:gracias.html");
+
+
+?>
